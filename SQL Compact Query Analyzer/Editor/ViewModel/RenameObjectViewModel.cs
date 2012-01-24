089ea0b12c9f2ff88884e5865bed24e29a063a99
christianhelle <christianhelle@localhost>
2012-01-24 23:09:11 +0100

    Removed dependency on MVVM Light

--- SQL Compact Query Analyzer/Editor/ViewModel/RenameObjectViewModel.cs
+++ SQL Compact Query Analyzer/Editor/ViewModel/RenameObjectViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using GalaSoft.MvvmLight;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ChristianHelle.DatabaseTools.SqlCe.QueryAnalyzer.ViewModel
 {
