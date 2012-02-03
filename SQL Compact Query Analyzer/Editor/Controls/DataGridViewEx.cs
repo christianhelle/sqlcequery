49af0fbd327b9705752d9a3aa022007db80e928a
christianhelle <christianhelle@localhost>
2012-02-03 21:46:51 +0100

    Displays NULL in italic if the value of the cell is null

--- SQL Compact Query Analyzer/Editor/Controls/DataGridViewEx.cs
+++ SQL Compact Query Analyzer/Editor/Controls/DataGridViewEx.cs
@@ -1,12 +1,38 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace ChristianHelle.DatabaseTools.SqlCe.QueryAnalyzer.Controls
 {
     public sealed class DataGridViewEx : DataGridView
     {
+        private Font italicFont;
+        private Font normalFont;
+
         public DataGridViewEx()
         {
             DoubleBuffered = true;
+            italicFont = new Font(Font.FontFamily, Font.Size, FontStyle.Italic);
+            normalFont = Font;
+
+            DefaultCellStyle.NullValue = "NULL";
+        }
+
+        public override Font Font
+        {
+            get { return base.Font; }
+            set
+            {
+                base.Font = value;
+                italicFont = new Font(Font.FontFamily, Font.Size, FontStyle.Italic);
+                normalFont = Font;
+            }
+        }
+
+        protected override void OnCellFormatting(DataGridViewCellFormattingEventArgs e)
+        {
+            e.CellStyle.Font = e.Value == DBNull.Value ? italicFont : normalFont;
+            base.OnCellFormatting(e);
         }
     }
 }
