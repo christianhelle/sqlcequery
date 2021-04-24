62ae88dd8e44c7d144372d101e1689f041627a7a
Olivier Diotte <olivier@diotte.ca>
2021-04-24 10:24:08 -0400

    Add Antlr4 TSql grammars and class
    The grammars are taken from the antlr-grammars project
    but modified slightly (file and grammar names are renamed from x to xCore)

---
+++ Source/TSqlParser/TSqlParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace ChristianHelle.DatabaseTools.SqlCe.TSqlParser {
+    class TSqlParser {
+
+        public static List<string> statements_from_str(string s) {
+            var stmts = new List<string>();
+
+            var stream = new AntlrInputStream(s.ToUpper());
+            ITokenSource lexer = new TSqlLexerCore(stream);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            TSqlParserCore parser = new TSqlParserCore(tokens);
+            parser.RemoveErrorListeners();
+            parser.RemoveParseListeners();
+            //parser.BuildParseTree = true;
+
+            if (parser.tsql_file() is var ctxt) {
+                foreach (var batch in ctxt?.children) {
+                    for (int i = 0; i < batch.ChildCount; i++) {
+                        var stmt = batch.GetChild(i);
+
+                        var stmt_s = "";
+                        var prefix = "";
+                        for (int j = stmt.SourceInterval.a; j <= stmt.SourceInterval.b; j++) {
+                            stmt_s += $"{prefix}{tokens.Get(j).Text}";
+                            prefix = " ";
+                        }
+
+                        stmts.Add(stmt_s);
+                    }
+                }
+            }
+
+            return stmts;
+        }
+    }
+}
